31e1c283576a44d8cd32650482ae1f6a002e0467
RGP <ro@192.168.1.10>
2025-09-27 21:58:31 -0600

    Adding templates and views

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('alumnos/', views.ventana_alumnos, name='alumnos')
+    path('alumnos/', views.ventana_alumnos, name='alumnos'),
+    path('alumnos_consulta', views.consulta_alumnos, name='consulta_alumnos')
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def login(request):
     return HttpResponse("Login de Usuarios")
+
+def crear_usuario(request):
+    return HttpResponse("Captura de Usuarios nuevos")
+
+def consulta_usuarios(request):
+    return HttpResponse("Consulta Usuarios")
